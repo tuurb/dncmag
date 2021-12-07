db8e7db1a8c12c920a6f3d8640d5b57fced0ea25
Arthur Brugts <arthur.brugts@greenchoice.nl>
2021-12-07 17:30:27 +0100

    intro until testing part

---
+++ 202111_NodeJSFundamentals/intro/hello_1.js
@@ -0,0 +1,4 @@
+const process = require("process");
+console.log("Hello world!");
+console.log(`The current time is ${ new Date() }`);
+console.log(`and I am running on the ${ process.platform } platform`);
